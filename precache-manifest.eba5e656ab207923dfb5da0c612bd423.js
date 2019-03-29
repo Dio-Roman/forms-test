9aba213bd2ce1ae655cbca496719183742be9834
unknown <dio-roman@mail.ru>
2019-03-29 18:31:15 +0300

    Updates

---
+++ precache-manifest.eba5e656ab207923dfb5da0c612bd423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65987dbe5ff966bedcda",
+    "url": "/forms-test/static/js/runtime~main.b799c449.js"
+  },
+  {
+    "revision": "6d56c4f69fa89bd0e22d",
+    "url": "/forms-test/static/js/main.64d572a8.chunk.js"
+  },
+  {
+    "revision": "a9a2489eff6062484a93",
+    "url": "/forms-test/static/js/2.2aaab720.chunk.js"
+  },
+  {
+    "revision": "6d56c4f69fa89bd0e22d",
+    "url": "/forms-test/static/css/main.73d4c470.chunk.css"
+  },
+  {
+    "revision": "f258a25f640d26563f23e29f54473130",
+    "url": "/forms-test/index.html"
+  }
+];
